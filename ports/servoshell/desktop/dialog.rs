9d72c9657a1d9c141597e6a2f5497412d198cae0
L Ashwin B <lashwinib@gmail.com>
2025-01-24 19:32:11 +0530

    Fix license header in dialog.rs to pass tidy check
    Signed-off-by: L Ashwin B <lashwinib@gmail.com>

--- ports/servoshell/desktop/dialog.rs
+++ ports/servoshell/desktop/dialog.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 use egui_file_dialog::{DialogState, FileDialog as EguiFileDialog};
 use log::warn;
 use servo::ipc_channel::ipc::IpcSender;
